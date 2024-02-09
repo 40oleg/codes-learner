46f504ff3a27ead88f475c2d063c770ad17fcba7
tors <oleg.merkulov701@gmail.com>
2024-02-10 00:23:25 +0300

    (chore): initializing application

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideAnimations(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule)],
+};
